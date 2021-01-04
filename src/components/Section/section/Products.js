4ce768b7e3fa7f23b72a248fac3b8aa17f41e96f
chicoumbetov <shyngys-maaan@mail.ru>
2021-01-05 04:56:24 +0600

    Home page, photo cards - section

--- src/components/Section/section/Products.js
+++ src/components/Section/section/Products.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { DataContext } from '../../../Context/Context'
 import '../css/Products.css'
+//import '../css/normalize.css'
 
 export default class Products extends Component {
 
@@ -11,28 +12,30 @@ export default class Products extends Component {
         const { products } = this.context;
 
         return (
-            <div id="product">
-                {
-                    products.map(product => (
-                        <div className="card" key={product._id}>
-                            <Link to={`/product/${product._id}`}>
-                                <img src={product.src} alt="" />
-                            </Link>
-                            <div className="content" >
-                                <h3>
-                                    <Link to={`/product/${product._id}`}>{product.title}</Link>
-                                </h3>
+            <div className="block">
+                <div id="product">
+                    {
+                        products.map(product => (
+                            <div className="card" key={product._id}>
+                                <Link to={`/product/${product._id}`}>
+                                    <img src={product.src} alt="" />
+                                </Link>
+                                <div className="content" >
+                                    <h3>
+                                        <Link to={`/product/${product._id}`}>{product.title}</Link>
+                                    </h3>
 
-                                <div className="pricelinethrough">
-                                    <span >{product.price} руб </span>
+                                    <div className="pricelinethrough">
+                                        <span >{product.price} руб </span>
+                                    </div>
+
+                                    <p>{product.price2} руб </p>
+                                    <button >Добавить в корзину</button>
                                 </div>
-
-                                <p>{product.price2} руб </p>
-                                <button >Добавить в корзину</button>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         )
     }
